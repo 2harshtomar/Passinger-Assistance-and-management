a7cde6bfeb4a65c7f8a17546a7eb0482729be93a
Harsh Tomar <2000078079@hexaware.com>
2023-03-29 18:52:18 +0530

    initial commit

---
+++ src/main/java/com/wcm/demo/model/Ssr.java
@@ -0,0 +1,73 @@
+package com.wcm.demo.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Ssr {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private LocalDateTime openDateTime;
+	private LocalDateTime closeDateTime;
+	private String status;
+	
+	@OneToOne
+	private PassengerDetails pssengerDetails;
+	
+	@OneToOne
+	private Staff staff;
+	
+	@OneToOne
+	private Wheel_Chair wheelChair;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public LocalDateTime getOpenDateTime() {
+		return openDateTime;
+	}
+	public void setOpenDateTime(LocalDateTime openDateTime) {
+		this.openDateTime = openDateTime;
+	}
+	public LocalDateTime getCloseDateTime() {
+		return closeDateTime;
+	}
+	public void setCloseDateTime(LocalDateTime closeDateTime) {
+		this.closeDateTime = closeDateTime;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public PassengerDetails getPssengerDetails() {
+		return pssengerDetails;
+	}
+	public void setPssengerDetails(PassengerDetails pssengerDetails) {
+		this.pssengerDetails = pssengerDetails;
+	}
+	public Staff getStaff() {
+		return staff;
+	}
+	public void setStaff(Staff staff) {
+		this.staff = staff;
+	}
+	public Wheel_Chair getWheelChair() {
+		return wheelChair;
+	}
+	public void setWheelChair(Wheel_Chair wheelChair) {
+		this.wheelChair = wheelChair;
+	}
+	
+	
+}
---
+++ src/main/java/com/wcm/demo/model/Staff.java
@@ -0,0 +1,42 @@
+package com.wcm.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Staff {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String staffCode;
+	private String status;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStaffCode() {
+		return staffCode;
+	}
+	public void setStaffCode(String staffCode) {
+		this.staffCode = staffCode;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
