59448d19d3e24cea198c73419080a1ec807ff127
Aaditya Mohan <aadimohan51@gmail.com>
2023-04-04 19:57:11 +0530

    api completed

--- src/main/java/com/wcm/demo/repository/SsrRepository.java
+++ src/main/java/com/wcm/demo/repository/SsrRepository.java
@@ -1,10 +1,10 @@
 package com.wcm.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.wcm.demo.model.Ssr;
 
 public interface SsrRepository extends JpaRepository<Ssr, Long>{
-	
+//	@Query("select s from Ssr s where s.staff.status = ?1")
+//	Ssr FindByStatus();
 
 }
