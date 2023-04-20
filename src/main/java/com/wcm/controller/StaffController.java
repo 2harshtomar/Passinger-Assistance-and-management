593182f7caf07cc79f9b9265e8cd7032df8e00e1
Harsh Tomar <2000078079@hexaware.com>
2023-04-20 14:31:27 +0530

    cross origin added

--- src/main/java/com/wcm/controller/StaffController.java
+++ src/main/java/com/wcm/controller/StaffController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ import com.wcm.service.StaffService;
 import com.wcm.service.StationRouterService;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RequestMapping("/api/staff")
 public class StaffController {
 	@Autowired
@@ -89,6 +91,7 @@ public class StaffController {
 
 		return ResponseEntity.status(HttpStatus.OK).body(resStaffDto);
 	}
+	// get all 
 	@GetMapping("/get/all")
 	public List<Staff> getAllStaff(Principal principal){
 		return staffService.getStaff(principal);
